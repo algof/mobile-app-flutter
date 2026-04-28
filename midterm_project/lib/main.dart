f9cc7ca34a1a74c583f21173a1df03621583ef40
algof <algofkz@gmail.com>
2026-04-28 21:16:32 +0700

    style: rapi-rapi sebelum fitur notification

--- midterm_project/lib/main.dart
+++ midterm_project/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'firebase_options.dart';
-import 'package:gal/gal.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
